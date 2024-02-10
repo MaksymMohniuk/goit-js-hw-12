50426e9126a715473fa7c26ca1676793439398bc
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-02-10 22:08:22 +0000

    Deploying to gh-pages from @ MaksymMohniuk/goit-js-hw-12@db6334ca6717f7ce913c757204a98d7ca4795a96 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,13 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import{S as l,i as u}from"./assets/vendor-5b791d57.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const d=document.querySelector("#searchInput"),f=document.querySelector('button[type="submit"]'),m=document.querySelector(".js-form"),c=document.querySelector(".gallery"),a=document.querySelector(".loader");let p=new l(".gallery a",{captionsData:"alt",captionDelay:250});f.addEventListener("click",y);function y(t){t.preventDefault();const s=d.value.trim();s&&(a.classList.add("is-shown"),m.reset(),g(s).then(r=>{r.hits.length?(a.classList.remove("is-shown"),b(r.hits)):u.error({title:"Sorry",message:"There are no images matching your search query. Please try again!",position:"topRight"})}).catch(r=>{console.error("Помилка:",r)}))}function g(t){const s=`https://pixabay.com/api/?key=33929638-3d09c2b606ca8b58d00360aed&q=${t}&image_type=photo?orientation=horizontal?safesearch=true`;return fetch(s).then(r=>{if(r.ok)return r.json();throw new Error(`Помилка: ${r.status}`)})}function h(t){return`
+  <li class="gallery-item">
+    <a href="${t.largeImageURL}">
+    <img src="${t.webformatURL}" alt="${t.tags}">
+    <div class="card-body">
+      <p class="card-text">Likes: ${t.likes}</p>
+      <p class="card-text">Views: ${t.views}</p>
+      <p class="card-text">Comments: ${t.comments}</p>
+      <p class="card-text">Downloads: ${t.downloads}</p>
+    </div>
+    </a>
+  </li>`}function b(t){c.innerHTML="";const s=t.map(r=>h(r)).join("");c.innerHTML=s,p.refresh()}
 //# sourceMappingURL=commonHelpers.js.map
