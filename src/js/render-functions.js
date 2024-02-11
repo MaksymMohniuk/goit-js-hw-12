a676ba677507d230e284cbab09f816d093223e92
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-02-11 22:43:18 +0200

    fetch-refactoring

---
+++ src/js/render-functions.js
@@ -0,0 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function photoTemplate(photo) {
+  return `
+    <li class="gallery-item">
+      <a href="${photo.largeImageURL}">
+      <img src="${photo.webformatURL}" alt="${photo.tags}">
+      <div class="card-body">
+        <p class="card-text">Likes: ${photo.likes}</p>
+        <p class="card-text">Views: ${photo.views}</p>
+        <p class="card-text">Comments: ${photo.comments}</p>
+        <p class="card-text">Downloads: ${photo.downloads}</p>
+      </div>
+      </a>
+    </li>`;
+}
+
+export function renderPhotos(photos) {
+  galleryRef.innerHTML = '';
+  const markup = photos.map(photo => photoTemplate(photo)).join('');
+  galleryRef.innerHTML = markup;
+  lightbox.refresh();
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getPhotos } from './js/pixabay-api';
+import { photoTemplate } from './js/render-functions';
+import { renderPhotos } from './js/render-functions';
 
 const inputRef = document.querySelector('#searchInput');
 const btnRef = document.querySelector('button[type="submit"]');
 const formRef = document.querySelector('.js-form');
-const galleryRef = document.querySelector('.gallery');
 const loaderRef = document.querySelector('.loader');
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
 
 btnRef.addEventListener('click', onButtonSubmit);
 
@@ -21,55 +17,21 @@ function onButtonSubmit(e) {
   if (!value) return;
   loaderRef.classList.add('is-shown');
   formRef.reset();
-  getPhotos(value)
-    .then(photos => {
-      if (!photos.hits.length) {
-        iziToast.error({
-          title: 'Sorry',
-          message:
-            'There are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-      } else {
-        loaderRef.classList.remove('is-shown');
-        renderPhotos(photos.hits);
-      }
-    })
-    .catch(error => {
-      console.error('Помилка:', error);
-    });
-}
-
-function getPhotos(data) {
-  const url = `https://pixabay.com/api/?key=33929638-3d09c2b606ca8b58d00360aed&q=${data}&image_type=photo?orientation=horizontal?safesearch=true`;
-
-  return fetch(url).then(res => {
-    if (res.ok) {
-      return res.json();
+  try {
+    const photos = getPhotos(value);
+    if (!photos.data.hits.length) {
+      iziToast.error({
+        title: 'Sorry',
+        message:
+          'There are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
     } else {
-      throw new Error(`Помилка: ${res.status}`);
+      loaderRef.classList.remove('is-shown');
+      photoTemplate(photo);
+      renderPhotos(photos.data.hits);
     }
-  });
-}
-
-function photoTemplate(photo) {
-  return `
-  <li class="gallery-item">
-    <a href="${photo.largeImageURL}">
-    <img src="${photo.webformatURL}" alt="${photo.tags}">
-    <div class="card-body">
-      <p class="card-text">Likes: ${photo.likes}</p>
-      <p class="card-text">Views: ${photo.views}</p>
-      <p class="card-text">Comments: ${photo.comments}</p>
-      <p class="card-text">Downloads: ${photo.downloads}</p>
-    </div>
-    </a>
-  </li>`;
-}
-
-function renderPhotos(photos) {
-  galleryRef.innerHTML = '';
-  const markup = photos.map(photo => photoTemplate(photo)).join('');
-  galleryRef.innerHTML = markup;
-  lightbox.refresh();
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
